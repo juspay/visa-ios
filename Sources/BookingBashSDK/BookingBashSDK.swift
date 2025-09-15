7670b2d1fef4e94cab13b530801b4cb8eb5877fa
Namit Goel <namit.goel@juspay.in>
2025-09-15 17:29:01 +0530

    fix: fixing ExperienceHomeView caller

--- Sources/BookingBashSDK/BookingBashSDK.swift
+++ Sources/BookingBashSDK/BookingBashSDK.swift
@@ -5,7 +5,13 @@ public struct BookingBashSDK {
     public init() {}
     
     public static func createExperienceHomeView(encryptPayLoad: String) -> some View {
-        ExperienceHomeView(encryptPayLoad: encryptPayLoad)
+        ExperienceHomeView(
+                encryptPayLoad: encryptPayLoad
+            ,   isActive: true
+            ,   onFinish: {
+                        print("Got callback")
+                }
+        )
     }
     
     public static func createMyTransactionView() -> some View {
