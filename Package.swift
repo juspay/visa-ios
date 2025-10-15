2202ea2fe9ce095de82727458435a4cf8bc4ad21
Namit Goel <namit.goel@juspay.in>
2025-10-15 14:54:17 +0530

    fix: hypersdk-ios version to 1.0.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "HyperSDK", url: "https://github.com/namitgoel/hypersdk-ios.git", branch: "new-sdk-test"),
+        .package(name: "HyperSDK", url: "https://github.com/namitgoel/hypersdk-ios.git", .exact("1.0.0")),
         .package(url: "https://github.com/ozontech/SUINavigation.git", from: "1.11.0"),
         .package(name: "SwiftUIIntrospect", url: "https://github.com/siteline/swiftui-introspect.git", from: "1.3.0"),
     ],
