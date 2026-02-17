4d9a80b1bba07e3f06d0fa7afa9ddab39dc73c3b
Smriti Grover <smriti.grover@juspay.in>
2026-02-17 14:59:34 +0530

    feat: PICAF-31268: decoupling of initiate process and adding support for banner offers

--- Sources/VisaBenefitsSDK/VisaBenefits.h
+++ Sources/VisaBenefitsSDK/VisaBenefits.h
@@ -19,16 +19,27 @@ typedef HyperEventsCallback VisaBenefitsEventsCallback;
 
 @end
 
-@interface VisaBenefits: HyperServices
+@interface VisaBenefits : NSObject
 
 @property (nonatomic, weak) id <VisaBenefitsDelegate> _Nullable delegate;
+@property (nonatomic, assign) BOOL shouldHideNavigationBarWhenPushed;
+@property (nonatomic, assign) BOOL shouldUseViewController;
 
 - (instancetype _Nonnull)initWithClientId:(NSString * _Nonnull)clientId region:(NSString * _Nonnull)region;
 
+- (void)initiate:(UIViewController * _Nonnull)viewController payload:(NSDictionary * _Nonnull)sdkPayload callback:(VisaBenefitsCallback _Nonnull)callback;
+
+- (void)process:(NSDictionary * _Nonnull)sdkPayload;
+
 - (void)show:(UIViewController * _Nonnull)viewController payload:(NSDictionary * _Nonnull)sdkPayload callback:(VisaBenefitsCallback _Nonnull)callback;
+
+- (BOOL)isInitialised;
+
+- (void)terminate;
 
 - (VisaBenefitsEventsCallback _Nullable)merchantEvent;
 
 @end
+
 
 NS_ASSUME_NONNULL_END
